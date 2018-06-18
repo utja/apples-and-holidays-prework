b9bc5440357dd90b75fa0276c45828ee31db4538
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-18 03:32:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supply|
       supplies = supplies.join(", ")
-      holiday = holiday.to_s.split("_")
+      holiday = holiday.to_s.split("_").collect do |i|
+        i.capitalize.join(" ") << ":"
+        puts 
 end
 
 def all_holidays_with_bbq(holiday_hash)
